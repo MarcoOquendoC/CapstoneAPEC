9eebad8d4a3bfd175b004434c2d56d4367a6672e
Marco Oquendo <marcooquendoc@gmail.com>
2022-07-29 02:17:49 -0500

    Fix navbar behavior

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,30 @@ const menuIcon = document.getElementsByClassName('toggle')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 const bars = document.getElementsByClassName('bar');
 const navbar = document.getElementById('navbar');
+const navs = document.getElementsByClassName('nav');
 
-menuIcon.addEventListener('click', () => {
+function toggle() {
   navbarLinks.classList.toggle('active');
   navbar.classList.toggle('active');
   bars[0].classList.toggle('active');
   bars[1].classList.toggle('active');
   bars[2].classList.toggle('active');
-});
+}
+
+function closeNav() {
+  navbarLinks.classList.remove('active');
+  navbar.classList.remove('active');
+  bars[0].classList.remove('active');
+  bars[1].classList.remove('active');
+  bars[2].classList.remove('active');
+}
+
+menuIcon.addEventListener('click', toggle);
+
+navs[0].addEventListener('click', closeNav);
+navs[1].addEventListener('click', closeNav);
+navs[2].addEventListener('click', closeNav);
+navs[3].addEventListener('click', closeNav);
 
 const speakers = [{
   name: 'Alex Gerfer',
